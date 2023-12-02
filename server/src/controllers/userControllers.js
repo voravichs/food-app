3bd9c128b54ad7570677e3a076cb21c5aa121101
Ken Yamada <yamakenth@gmail.com>
2023-12-02 16:30:55 -0500

    Modify user routes and controllers for updated queries

--- server/src/controllers/userControllers.js
+++ server/src/controllers/userControllers.js
@@ -4,6 +4,7 @@ const DEFAULT_PAGE = 0;
 const DEFAULT_PAGE_SIZE = 10;
 
 // Q5: Find a restaurant from the city that a given user has been to the most frequently
+/** @deprecated */
 const findRestaurantsInMostVisitedCity = (req, res) => {
   const { userId } = req.params;
   const { page = DEFAULT_PAGE, pageSize = DEFAULT_PAGE_SIZE } = req.query;
@@ -44,6 +45,7 @@ const findRestaurantsInMostVisitedCity = (req, res) => {
 };
 
 // Q6: Find most visited restaurant that the given user has been to
+/** @deprecated */
 const findMostVisitedRestaurants = (req, res) => {
   const { userId } = req.params;
   const { page = DEFAULT_PAGE, pageSize = DEFAULT_PAGE_SIZE } = req.query;
@@ -74,7 +76,128 @@ const findMostVisitedRestaurants = (req, res) => {
   );
 };
 
+// Q6: Returns all the information about the user.
+const getUserInfo = (req, res) => {
+  const { userId } = req.params;
+
+  if (!userId) {
+    const error = new Error("userId must be included in the params.");
+    error.statusCode = 400;
+    throw error;
+  }
+
+  connection.query(
+    `SELECT *
+    FROM user
+    WHERE user_id = '${userId}';`,
+    (err, data) => {
+      if (err || data.length === 0) {
+        console.log(`Error: ${err}`);
+        res.send([]);
+      } else {
+        res.send(data);
+      }
+    }
+  );
+};
+
+// Q7: Returns a list of the 5 most popular restaurants out of all the restaurants the user
+// has visited. Results are sorted by rating and number of reviews both in descending order.
+const findPopularBusinesses = (req, res) => {
+  const { userId } = req.params;
+
+  if (!userId) {
+    const error = new Error("userId must be included in the params.");
+    error.statusCode = 400;
+    throw error;
+  }
+
+  connection.query(
+    `SELECT
+      b.business_id,
+      b.name,
+      b.categories,
+      g.postal_code,
+      g.city,
+      g.state,
+      g.address,
+      b.stars,
+      b.review_count,
+      b.is_open
+    FROM Business b 
+      JOIN review r ON b.business_id = r.business_id
+      JOIN geographical_location g ON b.business_id = g.business_id
+    WHERE r.user_id = '${userId}'
+    ORDER BY b.review_count DESC
+    LIMIT 5;`,
+    (err, data) => {
+      if (err || data.length === 0) {
+        console.log(`Error: ${err}`);
+        res.send([]);
+      } else {
+        res.send(data);
+      }
+    }
+  );
+};
+
+// Q8: Returns a list of businesses the user has left reviews. Results are
+// sorted by the date and time of review creation in descending order.
+// TODO: implement function
+
+// Q9: Returns a list of 5 additional businesses from the city the user has been to
+// the most frequently. Results are sorted by rating and number of reviews both in
+// descending order.
+const findRecommendedBusinesses = (req, res) => {
+  const { userId } = req.params;
+
+  if (!userId) {
+    const error = new Error("userId must be included in the params.");
+    error.statusCode = 400;
+    throw error;
+  }
+
+  connection.query(
+    `WITH users_visits_most AS (
+      SELECT g.state, g.city
+      FROM review r JOIN geographical_location g
+        ON r.business_id = g.business_id
+      WHERE r.user_id = '${userId}'
+      GROUP BY g.state, g.city
+      ORDER BY COUNT(*)
+      LIMIT 1
+    )
+    SELECT
+      b.business_id,
+      b.name,
+      b.categories,
+      g.postal_code,
+      g.city,
+      g.state,
+      g.address,
+      b.stars,
+      b.review_count,
+      b.is_open
+    FROM Business b JOIN geographical_location g
+      ON b.business_id = g.business_id
+    WHERE g.city IN (SELECT city FROM users_visits_most)
+    ORDER BY b.stars DESC
+    LIMIT 5;`,
+    (err, data) => {
+      if (err || data.length === 0) {
+        console.log(`Error: ${err}`);
+        res.send([]);
+      } else {
+        res.send(data);
+      }
+    }
+  );
+};
+
 module.exports = {
   findRestaurantsInMostVisitedCity,
   findMostVisitedRestaurants,
+  getUserInfo,
+  findPopularBusinesses,
+  findRecommendedBusinesses,
 };
--- server/src/routes/userRoutes.js
+++ server/src/routes/userRoutes.js
@@ -6,5 +6,9 @@ const router = express.Router();
 
 router.get("/visitedInTopCity/:userId", users.findRestaurantsInMostVisitedCity);
 router.get("/mostVisited/:userId", users.findMostVisitedRestaurants);
+router.get("/:userId", users.getUserInfo);
+router.get("/:userId/popularBusinesses", users.findPopularBusinesses);
+// TODO: add Route 8
+router.get("/:userId/recommendedBusinesses", users.findRecommendedBusinesses);
 
 module.exports = router;
